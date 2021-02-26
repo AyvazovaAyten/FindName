3af873b907f3983f7fcf62c47e62c59f864a3a6a
unknown <aytanea@code.edu.az>
2021-02-26 18:38:46 +0400

    changed index.js

--- 26.02/index.js
+++ 26.02/index.js
@@ -1,11 +1,11 @@
-function sameLetter(name, testname) { 
+function sameLetter(name, testname) {
     let count = 0;
     let a = 0;
     for (let i = 0; i < testname.length; i++) {
         for (let j = a; j < name.length; j++) {
             if (testname.toLowerCase()[i] === name.toLowerCase()[j]) {
                 count++;
-                a = j+1;
+                a = j + 1;
                 break;
             }
         }
@@ -17,20 +17,21 @@ function findName(myarr, name) {
     let newarr = [];
     if (name.length >= 2) {
         myarr.forEach(value => {
-            newarr.push(sameLetter(value, name));
+            newarr.push(sameLetter(value, name) / value.length);
         });
         const maxcount = Math.max(...newarr);
-        if (maxcount >= 2) {
-            console.log(newarr);
+        if (maxcount * myarr[newarr.indexOf(maxcount)].length >= 2) {
             return myarr[newarr.indexOf(maxcount)];
         }
     }
     return "Not Found";
 }
-const names = ['Ayten', 'Aydan', 'Aysen', 'Qezenfer', 'Qelender', 'Gulten', 'Elvin', 'Pervin', 'Anar'];
+const names = ['Ayten', 'Aydan', 'Aysen', 'Qezenfer', 'esmer', 'Qelender', 'elsad', 'Gulten', 'Elvin', 'Pervin', 'Anar'];
 console.log(findName(names, 'evin'));
 console.log(findName(names, 'prvin'));
 console.log(findName(names, 'abbb'));
 console.log(findName(names, 'Qender'));
 console.log(findName(names, 'ayt'));
+console.log(findName(names, 'elsed'));
+console.log(findName(names, 'Wjk'));
 
